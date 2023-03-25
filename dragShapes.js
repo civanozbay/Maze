c82ccc0d726e147eeb9819a6cb78d245e6d0bddf
civanozbay <civan417@hotmail.com>
2023-03-25 12:19:26 +0100

    sides refactored according to width and height

---
+++ dragShapes.js
@@ -0,0 +1,47 @@
+const {Engine,Render,Runner,World,Bodies,MouseConstraint,Mouse} = Matter; // pull out objects from Matter library
+
+const width = 800 ;
+const height = 600 ;
+
+const engine =Engine.create(); // when we create engine world object come along with that
+const  { world } = engine;
+const render = Render.create({
+    // we are telling to render where we want to show our representation of everything
+    element : document.body,
+    engine : engine,
+    options : {
+        wireframes : false, // for solid shapes and giving color randomly
+        width : width,
+        height : height
+    }
+})
+Render.run(render);
+Runner.run(Runner.create(),engine);
+
+World.add(world,MouseConstraint.create(engine,{
+    mouse:Mouse.create(render.canvas)
+}))
+
+// walls
+const walls = [
+    Bodies.rectangle(400,0,800,40, { isStatic : true}),
+    Bodies.rectangle(400,600,800,40 ,{isStatic:true}),
+    Bodies.rectangle(0,300,40,600, {isStatic:true}),
+    Bodies.rectangle(800,300,40,600, {isStatic:true})
+]
+World.add(world,walls);
+
+//random shapes
+
+for(let i =0 ; i<30 ; i++){
+    if(Math.random() > 0.5){
+        World.add(world,Bodies.rectangle(Math.random() * width,Math.random() * height,50,50))
+    }else{
+        World.add(world,Bodies.circle(Math.random() * width,Math.random() * height, 35),{
+            render :{
+                fillStyle : 'red'
+            }
+        })
+    }
+}
+
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const {Engine,Render,Runner,World,Bodies,MouseConstraint,Mouse} = Matter; // pull out objects from Matter library
+const {Engine,Render,Runner,World,Bodies} = Matter; // pull out objects from Matter library
 
-const width = 800 ;
+const width = 600 ;
 const height = 600 ;
 
 const engine =Engine.create(); // when we create engine world object come along with that
@@ -10,7 +10,7 @@ const render = Render.create({
     element : document.body,
     engine : engine,
     options : {
-        wireframes : false, // for solid shapes and giving color randomly
+        wireframes : true, // for solid shapes and giving color randomly
         width : width,
         height : height
     }
@@ -18,31 +18,18 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(),engine);
 
-World.add(world,MouseConstraint.create(engine,{
-    mouse:Mouse.create(render.canvas)
-}))
 
 // walls
 const walls = [
-    Bodies.rectangle(400,0,800,40, { isStatic : true}),
-    Bodies.rectangle(400,600,800,40 ,{isStatic:true}),
-    Bodies.rectangle(0,300,40,600, {isStatic:true}),
-    Bodies.rectangle(800,300,40,600, {isStatic:true})
+    // first argument: left corner to middle of the page
+    // second  argument : how many units down from the top left corner
+    // third arg : width  // fourth arg : height
+    Bodies.rectangle(width/2,0,width,40, { isStatic : true}),
+    Bodies.rectangle(width/2,height,width,40 ,{isStatic:true}),
+    Bodies.rectangle(0,height/2,40,height, {isStatic:true}),
+    Bodies.rectangle(width,height/2,40,height, {isStatic:true})
 ]
 World.add(world,walls);
 
-//random shapes
 
-for(let i =0 ; i<30 ; i++){
-    if(Math.random() > 0.5){
-        World.add(world,Bodies.rectangle(Math.random() * width,Math.random() * height,50,50))
-    }else{
-        World.add(world,Bodies.circle(Math.random() * width,Math.random() * height, 35),{
-            render :{
-                fillStyle : 'red'
-            }
-        })
-
-    }
-}
 
